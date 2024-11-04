95e0ca3379fabd212336278c5b8fca9b04d515ab
user <user@email.com>
2024-11-04 06:58:01 +0530

    update 2024-11-04

--- publish/components.js
+++ publish/components.js
@@ -23,7 +23,17 @@ class AkhilFooter extends HTMLElement {
     this.innerHTML = `
     <footer>
         <div class="container">
-            <p style="text-align: center;">© 2024 Akhil N</p>
+          <div style="display: flex; flex-direction: row; align-items: center; justify-content: center; gap: 1rem">
+            <a href="https://github.com/akhiln28" target="_blank">
+              <img src="/github.svg" alt="github icon" height="16px" width="16px">
+              <span>akhiln28</span>
+            </a>
+            <a href="https://www.linkedin.com/in/akhiln28/" target="_blank">
+              <img src="/linkedin.svg" alt="linkedin icon" height="16px" width="16px">
+              <span>akhiln28</span>
+            </a>
+          </div>
+          <p style="text-align: center;">© 2024 Akhil N</p>
         </div>
     </footer>
 `;
